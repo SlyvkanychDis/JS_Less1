6b7b193c21f77c7282f9a56751c37578086751a7
DIS01~ <slvdimaslv@gmail.com>
2023-03-14 00:45:01 +0200

    NEW com

--- index.js
+++ index.js
@@ -33,7 +33,52 @@ console.log(15, isRound( 15 ) );
 console.log(20, isRound( 20 ) );
 console.log(44, isRound( 44 ) );*/
 
-function mod(a, b) {;
+/*function mod(a, b) {;
         return a % b;
 
+}*/
+
+/*function getMax(a, b){
+    if (a > b){
+        return a;
+    } else{
+        return b;
+    }
 }
+
+console.log('2 VS 5', getMax(2, 5));
+console.log('8 VS 5', getMax(8, 5));
+console.log('3 VS 5', getMax(3, 5));
+*/
+
+function checkNumInRange( num, start, end) {
+    return num > start && num < end;
+    
+ /*       if (num >start) {
+        if (num < end){
+            return true;
+        }else{
+            return false;
+        }
+    }else {
+        return false;
+    }
+    */
+}
+
+/* && (AND, * )
+true && true === true           1*1===1
+true && false === false         1*0
+false && true === false         0*1
+false && false ===false         0*0
+
+|| (OR, +)
+true || true === true           1+1 === 1
+true || false === true          1+0
+false || true === true          0+1
+false || false ===false         0+0 === 0
+*/
+
+console.log( '5 in 2 .....12', checkNumInRange( 5, 2, 12 ));
+console.log( '15 in 2 .....12', checkNumInRange( 15, 2, 12 ));
+console.log( '-5 in 2 .....12', checkNumInRange( -5, 2, 12 ));
